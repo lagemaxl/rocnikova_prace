9d0a2db6363adbf7d56e0a0f1e7f8fc0e57c50be
lagemaxl <lagemaxlcz@gmail.com>
2023-12-07 09:04:00 +0100

    delete trash

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -7,7 +7,7 @@ import {
   IconHome,
 } from "@tabler/icons-react";
 import classes from "~/style/NavbarSimple.module.css";
-import { Container, Group, Anchor } from "@mantine/core";
+import { Group } from "@mantine/core";
 import pb from "../lib/pocketbase";
 import { useNavigate } from 'react-router-dom';
 
@@ -16,80 +16,6 @@ const data = [
   { link: "", label: "Oznámení", icon: IconBellRinging },
   { link: "", label: "Nastavení", icon: IconSettings },
 ];
-
-const links = [
-  { link: "#", label: "Contact" },
-  { link: "#", label: "Privacy" },
-  { link: "#", label: "Blog" },
-  { link: "#", label: "Careers" },
-];
-
-export function FooterSimple() {
-  const items = links.map((link) => (
-    <Anchor<"a">
-      c="dimmed"
-      key={link.label}
-      href={link.link}
-      onClick={(event) => event.preventDefault()}
-      size="sm"
-    >
-      {link.label}
-    </Anchor>
-  ));
-
-  return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <h1>název</h1>
-        <Group className={classes.links}>{items}</Group>
-      </Container>
-    </div>
-  );
-}
-
-import {
-  TextInput,
-  TextInputProps,
-  ActionIcon,
-  useMantineTheme,
-  rem,
-} from "@mantine/core";
-import { IconSearch, IconArrowRight } from "@tabler/icons-react";
-
-export function InputWithButton(props: TextInputProps) {
-  const theme = useMantineTheme();
-
-  return (
-    <div className={classes.hled}>
-      <TextInput
-        radius="xl"
-        size="md"
-        placeholder="Vyhledej událost"
-        rightSectionWidth={42}
-        leftSection={
-          <IconSearch
-            style={{ width: rem(18), height: rem(18) }}
-            stroke={1.5}
-          />
-        }
-        rightSection={
-          <ActionIcon
-            size={32}
-            radius="xl"
-            color={theme.primaryColor}
-            variant="filled"
-          >
-            <IconArrowRight
-              style={{ width: rem(18), height: rem(18) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
-        }
-        {...props}
-      />
-    </div>
-  );
-}
 
 export default function NavbarSimple() {
   const [active, setActive] = useState("Domů");
@@ -130,8 +56,6 @@ useEffect(() => {
 
   return (
     <>
-      <InputWithButton />
-
       <nav className={classes.navbar}>
         <div className={classes.navbarMain}>
           <Group className={classes.header} justify="space-between">
